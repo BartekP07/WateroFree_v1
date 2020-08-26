cbe2b22328f508057eaf3f61c0f6fb58acdebba9
BartekP07 <pilch.bartlomiej984@gmail.com>
2020-08-26 22:52:53 +0200

    New version 17.08.20

---
+++ app/src/main/java/app/watero/waterofree/application/innerRecycler/HomeAdapter.java
@@ -0,0 +1,10 @@
+package app.watero.waterofree.application.innerRecycler;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeAdapter extends RecyclerView<HomeAdapter.HomeViewHolder> {
+
+
+    public class HomeViewHolder {
+    }
+}
